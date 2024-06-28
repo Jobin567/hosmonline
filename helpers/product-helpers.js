51d664745662cbbacd0ab0208f7952ac67364a86
Jobin George <141149299+Jobin567@users.noreply.github.com>
2024-06-28 11:02:54 +0530

    Update product-helpers.js

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,50 +1,81 @@
-var db=require('../config/connection')
-var collection=require('../config/collections')
-var objectId=require('mongodb').ObjectID
-module.exports={
-    addProduct:(product,callback)=>{
-        
-        db.get().collection('product').insertOne(product).then((data)=>{
-            console.log(data);
-            callback(data.ops[0]._id)
-        })
-    },
-    getAllProducts:()=>{
-        return new Promise(async(resolve,reject)=>{
-            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
-            resolve(products)
-        })
+const db = require('../config/connection');
+const collection = require('../config/collections');
+const { ObjectID } = require('mongodb');
+
+module.exports = {
+    addProduct: (product, callback) => {
+        db.get()
+            .collection(collection.PRODUCT_COLLECTION)
+            .insertOne(product)
+            .then((data) => {
+                console.log('Product added:', data.ops[0]);
+                callback(data.ops[0]._id);
+            })
+            .catch((err) => {
+                console.error('Error adding product:', err);
+                callback(null); // or handle error as needed
+            });
     },
 
-deleteProduct:(prodId)=>{
-    return new Promise((resolve,reject)=>{
-        db.get().collection(collection.PRODUCT_COLLECTION).removeOne({_id:objectId(prodId)}).then((response)=>{
-            //console.log(response);
-            resolve(response)
-        })
-    })
-},
-getProductDetails:(prodId)=>{
-    return new Promise((resolve,reject)=>{  
-        db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(prodId)}).then((product)=>{
-            resolve(product)
-        })
-})
-},
-updateProduct:(proId,proDetails)=>{
-    return new Promise((resolve,reject)=>{  
-        db.get().collection(collection.PRODUCT_COLLECTION)
-        .updateOne({_id:objectId(proId)},{
-            $set:{
-                Name:proDetails.Name,
-                Description:proDetails.Description,
-                Price:proDetails.Price,
-                Category:proDetails.Category,
-            }
-        }).then((response)=>{
-            resolve()
-        })
+    getAllProducts: async () => {
+        try {
+            let products = await db.get()
+                .collection(collection.PRODUCT_COLLECTION)
+                .find()
+                .toArray();
+            return products;
+        } catch (err) {
+            console.error('Error fetching products:', err);
+            return []; // or throw error as needed
+        }
+    },
 
-})
-}
-}
+    deleteProduct: async (prodId) => {
+        try {
+            let response = await db.get()
+                .collection(collection.PRODUCT_COLLECTION)
+                .deleteOne({ _id: ObjectID(prodId) });
+            return response;
+        } catch (err) {
+            console.error('Error deleting product:', err);
+            throw err; // or handle error as needed
+        }
+    },
+
+    getProductDetails: async (prodId) => {
+        try {
+            let product = await db.get()
+                .collection(collection.PRODUCT_COLLECTION)
+                .findOne({ _id: ObjectID(prodId) });
+            return product;
+        } catch (err) {
+            console.error('Error fetching product details:', err);
+            throw err; // or handle error as needed
+        }
+    },
+
+    updateProduct: async (proId, proDetails) => {
+        try {
+            let response = await db.get()
+                .collection(collection.PRODUCT_COLLECTION)
+                .updateOne(
+                    { _id: ObjectID(proId) },
+                    {
+                        $set: {
+                            Name: proDetails.Name,
+                            Description: proDetails.Description,
+                            Price: proDetails.Price,
+                            Category: proDetails.Category,
+                        },
+                    }
+                );
+            return response;
+        } catch (err) {
+            console.error('Error updating product:', err);
+            throw err; // or handle error as needed
+        }
+    },
+};
+
+      
+   
